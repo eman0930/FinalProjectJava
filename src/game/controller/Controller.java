effe7e8ae96cb61abcdac6adec1781fa60bd48a5
eman0930 <emanswimmer@gmail.com>
2023-04-19 07:53:49 -0600

    first commit

---
+++ src/game/controller/Controller.java
@@ -0,0 +1,9 @@
+package game.controller;
+
+public class Controller
+{
+	public void start()
+	{
+	
+	}
+}
---
+++ src/game/controller/Runner.java
@@ -0,0 +1,11 @@
+package game.controller;
+
+public class Runner
+{
+	public static void main (String [] args)
+	{
+		Controller app = new Controller ();//
+		app.start();
+	}
+}
+  
