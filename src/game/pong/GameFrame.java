b9619b644f95fbe2d7cb9e876743eca9b93c20a2
Ethan Leavitt <emanswimmer@gmail.com>
2023-04-25 15:10:45 -0600

    game Frame done

--- src/game/pong/GameFrame.java
+++ src/game/pong/GameFrame.java
@@ -11,6 +11,12 @@ public class GameFrame extends JFrame
 	{
 		panel = new GamePanel();
 		this.add(panel);
-		
+		this.setTitle("Pong game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
