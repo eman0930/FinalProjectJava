e80060c3bcad844e4ac4196f084ec79941cc10f4
Ethan Leavitt <emanswimmer@gmail.com>
2023-04-25 21:05:07 -0600

    added a lot to game panel

---
+++ src/game/pong/GamePanel.java
@@ -0,0 +1,77 @@
+package game.pong;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable
+{
+	static final int GAME_WIDTH = 1000;
+	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+	static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HIGHT);
+	static final int BALL_DIAMETER = 20;
+	static final int PADDLE_WIDTH = 25;
+	static final int PADDLE_HEIGHT = 100;
+	Thread gameThread;
+	Image image;
+	Graphics graphics;
+	Random random;
+	Paddle paddle1;
+	Paddle paddle2;
+	Ball ball;
+	Score score;
+	
+	GamePanel()
+	{
+		newPaddles();
+		newBall();
+		score = new Score(GAME_WIDTH,GAME_HEIGHT);
+		this.setFocusable(true);
+		this.addKeyListener(new AL());
+		this.setPreferredSize(SCREEN_SIZE);
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	public void newBall()
+	{
+		random = new Random();
+		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),random.nextInt(GAME_HEIGHT-BALL_DIAMETER),BALL_DIAMETER,BALL_DIAMETER);
+	}
+	
+	public void newPaddles()
+	{
+		paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
+		paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
